e5b7ecbc1c54a9e3d422b28cc37ea620370b558b
htothmartin <htothmartin@gmail.com>
2024-05-12 21:34:37 +0200

    Fix

---
+++ src/firebase.config.ts
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyAkDxRTH0feJ4UCEJ1g0jMm6ZxaVj4u5L4",
+  
+    authDomain: "webkert-mozi.firebaseapp.com",
+  
+    projectId: "webkert-mozi",
+  
+    storageBucket: "webkert-mozi.appspot.com",
+  
+    messagingSenderId: "967639920198",
+  
+    appId: "1:967639920198:web:38714ea7e3b2ee2bbda447",
+  
+    measurementId: "G-4ZW5H41Q8R"
+  
+  };
+  
